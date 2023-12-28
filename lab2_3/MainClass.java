403fe7172e8388e9b2cbed5c955ef33f7fa557ad
AlinaaZaharia <146974903+AlinaaZaharia@users.noreply.github.com>
2023-12-28 19:51:11 +0200

    Add files via upload

---
+++ lab2_3/MainClass.java
@@ -0,0 +1,16 @@
+package lab2_4;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		String str1 = "Azi e miercurea ";
+		String str2 = "maimutelor";
+		String str3 = str1+str2;
+		System.out.println(str3.equals("Azi e miercurea maimutelor"));
+		
+		int numar = 3;
+		System.out.println(numar == 3);
+	}
+
+}
